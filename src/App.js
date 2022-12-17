69f748955ae08cece36a45862590fd3a2399da6a
BELINDA <kennyegun240@gmail.com>
2022-12-17 16:33:04 +0100

    Use router to switch between pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Register } from './auth/register';
+import Login from './auth/Login';
+import { useContext } from 'react';
+import { AuthContext } from './context/context';
+import Homepage from './auth/homepage';
 
 function App() {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route
+              index
+              element={currentUser ? <Homepage /> : <Login />}
+            />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
