a837d720e0f5db88403a03ce28b75431a5eb57d7
BELINDA <kennyegun240@gmail.com>
2022-12-17 16:39:14 +0100

    App body

---
+++ src/auth/homepage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Center from '../pages/Center';
+import Leftside from '../pages/Leftside';
+import Rightside from '../pages/Rightside';
+
+const Homepage = () => {
+  return (
+    <div className='home'>
+      <div className="left">
+        <Leftside className="left"/>
+      </div>
+      <div className='center'>
+        <Center />
+        <Center />
+      </div>
+      <div className='right'>
+        <Rightside />
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
