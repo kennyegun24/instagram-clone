5801508c7a74c68c0a29d57fbb693ad0a9bd67f2
BELINDA <kennyegun240@gmail.com>
2022-12-17 16:33:34 +0100

    Modify

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,19 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "instagram-clone-a49ad.firebaseapp.com",
-  projectId: "instagram-clone-a49ad",
-  storageBucket: "instagram-clone-a49ad.appspot.com",
-  messagingSenderId: "393827576103",
-  appId: "1:393827576103:web:28ffe078349fab58f96102"
+  // apiKey: "AIzaSyDHnK7QqLNrW2yVLsFLH9MlS3Q1c5lHVLI",
+  // authDomain: "instagram-clone-a49ad.firebaseapp.com",
+  // projectId: "instagram-clone-a49ad",
+  // storageBucket: "instagram-clone-a49ad.appspot.com",
+  // messagingSenderId: "393827576103",
+  // appId: "1:393827576103:web:28ffe078349fab58f96102"
+
+  apiKey: "AIzaSyD6d-IfAnwHOxfwl7GuczfOBbSTAoqQNR4",
+  authDomain: "instagram-4528b.firebaseapp.com",
+  projectId: "instagram-4528b",
+  storageBucket: "instagram-4528b.appspot.com",
+  messagingSenderId: "914494289927",
+  appId: "1:914494289927:web:0f32a965060400b15992a2"
 };
 
 // Initialize Firebase
