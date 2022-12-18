247430ee9fd798b42d445539a0c05f3046851a62
BELINDA <kennyegun240@gmail.com>
2022-12-18 23:03:48 +0100

    Full authentication

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Login from './auth/Login';
 import { useContext } from 'react';
 import { AuthContext } from './context/context';
 import Homepage from './auth/homepage';
+import Leftside from './pages/Leftside';
+import Messages from './pages/Messages';
+import UserProfile from './pages/userProfile';
+import Search from './components/left/Search';
 
 function App() {
   const {currentUser} = useContext(AuthContext)
@@ -13,16 +17,32 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/'>
-            <Route
-              index
-              element={currentUser ? <Homepage /> : <Login />}
-            />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-          </Route>
-        </Routes>
+            {currentUser ? 
+            <div className='center'>
+              <div className='hey' >
+                <div className='side'>
+                  <Leftside />
+                </div>
+                <Routes>
+                  <Route path='/'>
+                    <Route
+                      index
+                      element={<Homepage />}
+                    />
+                    <Route path='login' element={<Login />} />
+                    <Route path='messages' element={<Messages />} />
+                    <Route path='search' element={<Search />} />
+                    <Route path={currentUser ? `${currentUser.displayName}` : '/'} element={<UserProfile />} />
+                  </Route>
+                </Routes>
+              </div>
+            </div>
+            :
+            <Routes>
+              <Route index element={<Login />} />
+              <Route path='login' element={<Login />} />
+              <Route path='register' element={<Register />} />
+            </Routes> }
       </BrowserRouter>
     </div>
   );
